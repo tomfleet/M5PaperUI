16f656809f429aaa18610073c8a2793e005dd1f2
Tom Fleet <tomfleet@gmail.com>
2022-02-17 00:40:38 +0000

    Update convert.py
    kludged my way to making this work on py3.6 & win11

--- lib/Resources/convert.py
+++ lib/Resources/convert.py
@@ -1,7 +1,10 @@
 import sys
 from PIL import Image, ImageOps
 import os.path
-import fs
+import fs #import list
+from fs import open_fs
+
+from pathlib import Path
 
 GRAYSCALE_MAP = [0 for i in range(256)]
 
@@ -29,29 +32,42 @@ def main():
 
   this_dir = os.path.dirname(__file__)
   src_dir = os.path.join(this_dir, "..", "..", "images")
-
+  print(src_dir)
+  full_path=src_dir
+  print(str(Path(full_path).parents[4]))
+  src_dir=str(Path(full_path).parents[4])+"\images"
+  print(src_dir)
   dstf = open('image_resources.h', 'w+')
   dstf.write("#pragma once\n")
 
-
-  for f in fs.list(path=src_dir):
-    final = Image.open(f)
-    ident = "_".join(os.path.basename(f).split(".")[:-1]).upper()
-    dstf.write("\nconst unsigned char %s[] PROGMEM = {\n" % (ident))
-    buffer = getBitmap(final)
-    cnt = 0
-    for byte in buffer:
-      cnt += 1
-      if cnt == 1:
-        dstf.write("    ")
-        dstf.write('0x%02X' % byte)
-      elif cnt == 12:
-        cnt = 0
-        dstf.write(', 0x%02X,' % byte)
-        dstf.write('\n')
-      else:
-        dstf.write(', 0x%02X' % byte)
-    dstf.write('};\n')
+  with open_fs(src_dir) as fs:
+      print(src_dir)
+      print(fs)
+      
+      for f in fs.walk.files():
+            print(src_dir+f)
+            tmp=src_dir+f
+            print(tmp)
+            tmp2 = tmp.replace("/","\\")
+            print(tmp2)
+            final = Image.open(tmp2)
+            ident = "_".join(os.path.basename(tmp2).split(".")[:-1]).upper()
+            print(ident)
+            dstf.write("\nconst unsigned char %s[] PROGMEM = {\n" % (ident))
+            buffer = getBitmap(final)
+            cnt = 0
+            for byte in buffer:
+              cnt += 1
+              if cnt == 1:
+                dstf.write("    ")
+                dstf.write('0x%02X' % byte)
+              elif cnt == 12:
+                cnt = 0
+                dstf.write(', 0x%02X,' % byte)
+                dstf.write('\n')
+              else:
+                dstf.write(', 0x%02X' % byte)
+            dstf.write('};\n')
 
 
 
